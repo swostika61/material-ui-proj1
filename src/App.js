293054d1664ebf73c4db30069b7283658d23abdc
swostika61 <swostikalamichhane61@gmail.com>
2021-04-07 21:11:26 +0545

    worked with  grid typography and theme

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import Header from './Header';
+
+const App = () => {
+  return (
+    <Grid container direction="column">
+      <Grid item> <Header/> </Grid>
+      <Grid item container>
+        <Grid item xs={0} sm={2}  />
+        <Grid item xs={12} sm={8}>hi</Grid>
+        <Grid item xs={0} sm={2} />
+      </Grid>
+    </Grid>
+  );
+}
+
+export default App;
